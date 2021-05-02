36bc8c92ff520911797becb195676aa8ea474c0e
mauraqp <mauraqoonitah@gmail.com>
2021-05-02 15:11:01 +0700

    routing request

--- server.js
+++ server.js
@@ -4,25 +4,69 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
+    // const { method } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>');
+    // if (method === 'GET') {
+    //     response.end('<h1>Hello!</h1>');
+    // }
+    // if (method === 'POST') {
+    //     let body = [];
+
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
+
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         //mengubah JSON string menjadi JavaScript objek
+    //         const { name } = JSON.parse(body);
+    //         response.end(`<h1>Hai! ${name}!</h1>`);
+    //     });
+    // }
+
+    //===========
+    // Routing Request
+    //===========
+
+    //ambil properti url dari request menggunakan teknik destructuring object
+
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+
+        // TODO 2: logika respons bila url bernilai '/'
+    } else if (url === '/about') {
+        // TODO 3: logika respons bila url bernilai '/about'
+        if (method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+        }
+
+    } else {
+        // TODO 1: logika respons bila url bukan '/' atau '/about'
+        response.end('<h1> Halaman tidak ditemukan!</h1>');
     }
-    if (method === 'POST') {
-        let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            //mengubah JSON string menjadi JavaScript objek
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai! ${name}!</h1>`);
-        });
-    }
+
 };
 
 const server = http.createServer(requestListener);
